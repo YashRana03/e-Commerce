047e461d92ebf456899cd95af85c8b5cf95af6df
Yash rana <97224327+YashRana03@users.noreply.github.com>
2023-10-06 17:36:58 +0100

    Add complete cart functionality

---
+++ data.js
@@ -0,0 +1,21 @@
+
+export const everything = {
+    0: {image: "./images/product-1.png", text: "Pink Adidas", price: 45},
+    1: {image: "./images/product-2.png", text: "Red Nikes", price: 50},  
+    2: {image: "./images/product-3.png", text: "High Heels", price: 35},
+    3: {image: "./images/product-4.png", text: "Convers Black", price: 30},
+    4: {image: "./images/product-5.png", text: "Red handbag", price: 70},
+    5: {image: "./images/product-6.png", text: "LV Pink Handbag", price: 150},
+    6: {image: "./images/product-7.png", text: "Gucci Handbag", price: 130},
+    7: {image: "./images/product-8.png", text: "Prada Handbag", price: 110},
+    8: {image: "./images/product-9.png", text: "Gucci Watch", price: 140},
+    9: {image: "./images/product-10.png", text: "Boss Watch", price: 135},
+    10: {image: "./images/product-11.png", text: "Armani Watch", price: 155},
+    11: {image: "./images/product-12.png", text: "Guess Watch", price: 175},
+    12: {image: "./images/product-13.png", text: "Gucci Glasses", price: 75},
+    13: {image: "./images/product-14.png", text: "Chanel Glasses", price: 65},
+    14: {image: "./images/product-15.png", text: "Versace Glasses", price: 70},
+    15: {image: "./images/product-16.png", text: "Armani Glasses", price: 80},
+    
+}
+
